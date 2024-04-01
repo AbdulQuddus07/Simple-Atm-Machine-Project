833ba93b218c5732efc9ef5219efb63c58b0c81d
Abdul Quddus <abdulquddus33kk@gmail.com>
2024-04-02 01:34:43 +0500

    updated done

--- index.ts
+++ index.ts
@@ -75,4 +75,4 @@ if (pinAnswer.pin === myPIN) {
 } else {
   console.log(chalk.red("Pin is Incorrect, Try Again!"));
 }
-console.log(chalk.italic.bold.gray("\n \t-:Thank You! for coming here:-\n"));
+console.log(chalk.italic.bold.gray("\n \t-:Thank You! For Coming Here:-\n"));
